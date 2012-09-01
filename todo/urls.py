731bd4e412736c5692dfab691e3c85c634d1716f
Laurent Peuch <cortex@worlddomination.be>
2012-09-02 00:39:54 +0200

    [mod] extract view code to views.py in todo app

--- todo/urls.py
+++ todo/urls.py
@@ -2,28 +2,8 @@ from django.conf.urls import patterns, url
 from django.views.generic import ListView
 from django.http import HttpResponse
 
-from djangbone.views import BackboneAPIView
-
 from models import Todo
-from forms import AddTodoForm, EditTodoForm
-
-
-class TodoBackboneView(BackboneAPIView):
-    base_queryset = Todo.objects.all()
-    add_form_class = AddTodoForm
-    edit_form_class = EditTodoForm
-
-    def get_collection(self, request, *args, **kwargs):
-        """
-        Handle a GET request for a full collection (when no id was provided).
-        """
-        qs = self.base_queryset[:10]
-        output = self.serialize_qs(qs)
-        return self.success_response(output)
-
-    def validation_error_response(self, form_errors):
-        print form_errors
-        from ipdb import set_trace; set_trace()
+from views import TodoBackboneView
 
 
 def dummy(request):
