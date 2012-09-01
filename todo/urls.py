a2e06e275a9c0d5dfd3986a51805e6b7b873f575
Laurent Peuch <cortex@worlddomination.be>
2012-09-02 00:40:05 +0200

    [mod] remove useless code

--- todo/urls.py
+++ todo/urls.py
@@ -1,18 +1,12 @@
 from django.conf.urls import patterns, url
 from django.views.generic import ListView
-from django.http import HttpResponse
 
 from models import Todo
 from views import TodoBackboneView
 
 
-def dummy(request):
-    return HttpResponse("prout")
-
-
 urlpatterns = patterns('',
     url(r'^$', ListView.as_view(model=Todo), name='home'),
-    url(r'^add/$', dummy, name='add'),
     url(r'^todo/$', TodoBackboneView.as_view(), name="backbone_todo"),
     url(r'^todo/(?P<id>\d+)/$', TodoBackboneView.as_view(), name="backbone_one_todo"),
 )
