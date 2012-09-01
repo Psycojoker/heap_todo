13d00a36599bbcd237c3d6cdcda71f26b062dce9
Laurent Peuch <cortex@worlddomination.be>
2012-09-02 01:42:24 +0200

    [mod] refactoring, remove useless ListView and do a TemplateView instead

--- todo/urls.py
+++ todo/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import patterns, url
-from django.views.generic import ListView
+from django.views.generic import TemplateView
 
-from models import Todo
 from views import TodoBackboneView
 
 
 urlpatterns = patterns('',
-    url(r'^$', ListView.as_view(model=Todo), name='home'),
+    url(r'^$', TemplateView.as_view(template_name="home.haml"), name='home'),
     url(r'^todo/$', TodoBackboneView.as_view(), name="backbone_todo"),
     url(r'^todo/(?P<id>\d+)/$', TodoBackboneView.as_view(), name="backbone_one_todo"),
 )
