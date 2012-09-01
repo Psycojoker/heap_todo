4bf2f7f3f28300dc8baea54f99782d5a1b460114
Laurent Peuch <cortex@worlddomination.be>
2012-09-02 00:40:45 +0200

    [mod] refactoring, remove an indentation

--- todo/forms.py
+++ todo/forms.py
@@ -6,10 +6,10 @@ class AddTodoForm(forms.Form):
     title = forms.CharField()
 
     def save(self, *args, **kwargs):
-        if self.is_valid():
-            return Todo.objects.create(title=self.cleaned_data["title"], position=1, page=1)
-        else:
+        if not self.is_valid():
             raise Exception
+
+        return Todo.objects.create(title=self.cleaned_data["title"], position=1, page=1)
 
 
 class EditTodoForm(forms.ModelForm):
