4f347656138be850c781b91469953436faf4cb78
Laurent Peuch <cortex@worlddomination.be>
2012-09-01 07:09:11 +0200

    [enh] add name to BackboneApi urls for the Todo model

--- todo/urls.py
+++ todo/urls.py
@@ -1,19 +1,36 @@
 from django.conf.urls import patterns, url
 from django.views.generic import ListView
 from django.http import HttpResponse
+from django import forms
+
 from djangbone.views import BackboneAPIView
 
 from models import Todo
 
+
+class AddTodoForm(forms.Form):
+    title = forms.CharField()
+
+    def save(self, *args, **kwargs):
+        if self.is_valid():
+            return Todo.objects.create(title=self.cleaned_data["title"], position=1, page=1)
+        else:
+            raise Exception
+
+
 class TodoBackboneView(BackboneAPIView):
     base_queryset = Todo.objects.all()
+    add_form_class = AddTodoForm
+    #edit_form_class = AddTodoForm
+
 
 def dummy(request):
     return HttpResponse("prout")
 
+
 urlpatterns = patterns('',
     url(r'^$', ListView.as_view(model=Todo), name='home'),
     url(r'^add/$', dummy, name='add'),
-    url(r'^todo/$', TodoBackboneView.as_view()),
-    url(r'^todo/(?P<id>\d+)/$', TodoBackboneView.as_view()),
+    url(r'^todo/$', TodoBackboneView.as_view(), name="backbone_todo"),
+    url(r'^todo/(?P<id>\d+)/$', TodoBackboneView.as_view(), name="backbone_one_todo"),
 )
