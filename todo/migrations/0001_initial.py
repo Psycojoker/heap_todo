3f27ff8d88a61b97b26043d08d018b84420e1ef8
Laurent Peuch <cortex@worlddomination.be>
2012-09-02 04:09:36 +0200

    [enh] move todo app to south

---
+++ todo/migrations/0001_initial.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Todo'
+        db.create_table('todo_todo', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('done', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('position', self.gf('django.db.models.fields.IntegerField')(null=True)),
+            ('page', self.gf('django.db.models.fields.IntegerField')()),
+        ))
+        db.send_create_signal('todo', ['Todo'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Todo'
+        db.delete_table('todo_todo')
+
+
+    models = {
+        'todo.todo': {
+            'Meta': {'ordering': "['position']", 'object_name': 'Todo'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'done': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'page': ('django.db.models.fields.IntegerField', [], {}),
+            'position': ('django.db.models.fields.IntegerField', [], {'null': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        }
+    }
+
+    complete_apps = ['todo']
